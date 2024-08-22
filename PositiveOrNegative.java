1bd146669cf214dc4b79784a1cd80b97dbd88663
“sk174” <shubhamkokate174@gmail.com>
2024-08-22 14:46:25 +0530

    positive number

---
+++ PositiveOrNegative.java
@@ -0,0 +1,13 @@
+public class PositiveOrNegative {
+    public static void main(String[] args) {
+        int a=1;
+        if(a>0){
+            System.out.println("positive Number");
+        }
+        else{
+            System.out.println("negative Number");
+        }
+    }
+
+
+}
